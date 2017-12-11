29eae6eaf1a084735b94adf22d16173d43f17961
sunny <87704470@qq.com>
2017-12-12 06:48:26 +0800

    first commit

---
+++ test/src/main/java/cc/iriding/test/XXKK.java
@@ -0,0 +1,13 @@
+package cc.iriding.test;
+
+import android.util.Log;
+
+/**
+ * Created by sunny on 2017/12/9.
+ */
+
+public class XXKK {
+    public void read() {
+        Log.i("XXX", "111111");
+    }
+}
